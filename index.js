2a247e3acacf78df6bf1e5b1cd438205c7d25556
Jesse Lampert <jesse.lampert@gmail.com>
2017-04-20 11:28:57 -0400

    tweaks

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var basicAuth = require('basic-auth');
 var moment = require('moment');
 var methodOverride = require('method-override');
+var request = require('request');
 
 //
 // create the express server
@@ -13,36 +14,32 @@ var app = express( );
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-//
-// setup web endpoints
-//
-var auth = function (req, res, next) {
-  function unauthorized(res) {
-    console.log( "unauthorized " + req.url );
-    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-    return res.sendStatus(401);
-  };
-
-  var user = basicAuth(req);
-
-  if (!user || !user.name || !user.pass) {
-    return unauthorized(res);
-  };
-
-  if( user.name === process.env.ADMIN_AUTH_USER && user.pass === process.env.ADMIN_AUTH_PASSWORD ) {
-    return next();
-  } else {
-    return unauthorized(res);
-  };
-};
-
 //
 // Global app configuration section
 //
 app.set('views', './cloud/views'); // Specify the folder to find templates
 app.set('view engine', 'ejs'); // Set the template engine
 app.use(express.static('public'));
+
+app.get('/oauth', function(req, res) {
+  if(!req.query.code) {
+    res.status(500);
+    res.send({"Error": "Looks like we're not getting code."});
+    console.log("Looks like we're not getting code.");
+  } else {
+    request({
+      url: 'https://slack.com/api/oauth.access',
+      qs: {code: req.query.code, client_id: process.env.CLIENT_ID, client_secret: process.env.CLIENT_SECRET},
+      method: 'GET'
+    }, function(error, response, body) {
+      if(error) {
+        console.log(error);
+      } else {
+        res.json(body);
+      }
+    })
+  }
+});
 
 //
 // view an app
